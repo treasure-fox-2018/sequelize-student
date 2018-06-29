1c9c04a2dd9ba72a7a2b35cd1f05d7c79a23d698
sumarsanaadi <sumarsana.adi@gmail.com>
2018-06-29 19:52:28 +0700

    finish release 4

--- index.js
+++ index.js
@@ -6,22 +6,44 @@ let Model = require ('./models')
 // Model.Student.findAll()
 //   .then((Students)=>{
 //     Students.forEach(student=>{
-//       console.log(student.getAge())
 //       console.log(student.getFullName())
+//       console.log(student.getAge())
 //     })
 //   }).catch((err) => {
 //     console.log("error message: ",err)
 //   })
 
 //RELEASE2
-var all = Model.Student.getFemaleStudent(function (femaleStudents) {
-  femaleStudents.forEach(femaleStudent => {
-    console.log("id :",femaleStudent.id)
-    console.log("first_name :",femaleStudent.first_name)
-    console.log("last_name :",femaleStudent.last_name)
-    console.log("full_name :",femaleStudent.full_name)
-    console.log("age :",femaleStudent.age)
-    console.log("")
-  })
-})
+// var all = Model.Student.getFemaleStudent ()
+//   .then (femaleStudents => {
+//     femaleStudents.forEach(femaleStudent => {
+//       console.log("id :",femaleStudent.id)
+//       console.log("first_name :",femaleStudent.first_name)
+//       console.log("last_name :",femaleStudent.last_name)
+//       console.log("full_name :",femaleStudent.full_name)
+//       console.log("age :",femaleStudent.age)
+//       console.log("------------------------")
+//     })
+//   }).catch((err) => {
+//     console.log("error message: ",err)
+//   })  
 
+//RELEASE 3 & 4 validate data 
+Model.Student.create(
+  {
+    first_name: "foo",
+    last_name: "bar",
+    gender: "male",
+    birthday: "1990-12-12",
+    email: "a21@bar.com",
+    phone: "123456789!",
+    height : 160,
+    email2: "b21@foo.com",
+    phone2: "23193819a"
+  }).then ( () => {
+    console.log("selesai")
+  })
+  .catch (err => {
+    console.log("error : ",err)
+  })
+    
--- models/student.js
+++ models/student.js
@@ -4,10 +4,79 @@ module.exports = (sequelize, DataTypes) => {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     gender: DataTypes.STRING,
-    birthday: DataTypes.DATE,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING
-  }, {});
+    birthday: DataTypes.DATEONLY,
+    email: {
+      validate: {
+        isEmail: true,
+        is : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
+        isUnique : function (value,next) {
+                Student.findOne({
+                  where: {email: value}
+                }).then (result => {
+                  if (result !== null) {
+                    return next("email already in use")
+                  } else next ()
+                  
+                }).catch (err => {
+                  return next("error message: ",err)
+                })
+             }
+      },
+      type: DataTypes.STRING,
+    },
+    phone: {
+      validate: {
+        len : { args : [10,13] , msg : "Phone length must be 10 -13"}
+      },
+      validate: {
+        not : { args : ["[a-z]",'i'] , msg : "Phone not allow letter"}
+      },
+      validate: {
+        is : { args : /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?[\-\.\ \\\/]?(\d+))?$/ , msg : "Phone not allow alphanumeric"}
+      },
+      type : DataTypes.STRING
+    },
+    height : {
+      validate: {
+        min: { args: [150], msg: "minimum length 150 cm"}
+      },
+      type: DataTypes.INTEGER
+    },
+    email2: {
+      validate: {
+        isEmail: true,
+        is : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i,
+        isUnique : function (value,next) {
+              Student.findOne({
+                where: {email2: value}
+              }).then (result => {
+                if (result !== null) {
+                  return next("email already in use")
+                } else next ()
+                
+              }).catch (err => {
+                return next("error message: ",err)
+              })
+       }
+      },
+      type: DataTypes.STRING
+    },
+    phone2: {
+      validate: {
+        len : { args : [10,13] , msg : "Phone length must be 10 -13"}
+      },
+      validate: {
+        not : { args : ["[a-z]",'i'] , msg : "Phone not allow letter"}
+      },
+      validate: {
+        is : { args : /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?[\-\.\ \\\/]?(\d+))?$/ , msg : "Phone not allow alphanumeric"}
+      },
+      type : DataTypes.STRING
+    }
+  },
+  {
+
+  });
   Student.associate = function(models) {
     // associations can be defined here
   };
@@ -18,27 +87,32 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   Student.prototype.getAge = function () {
-    var ageDifMs = Date.now() - new Date (this.birthday);
-    var ageDate = new Date(ageDifMs);
+    var ageDif = Date.now() - new Date (this.birthday);
+    var ageDate = new Date(ageDif);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
 
   //class method
-  Student.getFemaleStudent = function (cb) {
-    Student.findAll(
-      { where : {
-          gender : "female"
-        }
-      },
-      {raw:true}
-    )
-    .then (femaleStudents => {
-      femaleStudents.forEach(femaleStudent => {
-        femaleStudent.full_name = femaleStudent.getFullName()
-        femaleStudent.age = femaleStudent.getAge()
+  Student.getFemaleStudent = function () {
+    return new Promise ((resolve, reject) => {
+      Student.findAll(
+        { where : {
+            gender : "female"
+          }
+        },
+        {raw:true}
+      )
+      .then (femaleStudents => {
+        femaleStudents.forEach(femaleStudent => {
+          femaleStudent.full_name = femaleStudent.getFullName()
+          femaleStudent.age = femaleStudent.getAge()
+        })
+        resolve (femaleStudents)
+      }).catch((err) => {
+        reject(err)
       })
-      cb (femaleStudents)
     })
+    
   }
 
   return Student;
